6593f56e1c0158b97f8f8a0d5ea6c90500178604
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-09 18:36:51 +1000

    Fix linting

--- modules/pretty_permalinks/manifests/init.pp
+++ modules/pretty_permalinks/manifests/init.pp
@@ -1,14 +1,15 @@
+# A class to set the permalinks for WordPress.
 class pretty_permalinks (
   $config
 ) {
   if $config[permalink] {
-    $permalink_scheme = $config[permalink]
+	$permalink_scheme = $config[permalink]
 
-    wp::command { 'wp rewrite structure':
-      location => $config[mapped_paths][base],
-      command  => "rewrite structure '${ $permalink_scheme }'",
-      # These tasks will not run unless WP is installed.
-      require  => Chassis::Wp[ $config['hosts'][0] ],
-    }
+	wp::command { 'wp rewrite structure':
+	  location => $config[mapped_paths][base],
+	  command  => "rewrite structure '${ $permalink_scheme }'",
+	  # These tasks will not run unless WP is installed.
+	  require  => Chassis::Wp[ $config['hosts'][0] ],
+	}
   }
 }
